20539390260352f596b0fa7938bb1a967edcdaa1
Hamid <39879279+hamid93asd@users.noreply.github.com>
2023-07-06 15:07:23 -0600

    Update kill process.py

--- killing proccess/kill process.py
+++ killing proccess/kill process.py
@@ -1,7 +1,9 @@
 import psutil
 
+appName = "firefox.exe"    # enter application name
+
 print("running...")
-process = filter(lambda p: p.name() == "firefox.exe", psutil.process_iter())
+process = filter(lambda p: p.name() == appName, psutil.process_iter())
 for i in process:
     print(i.name,i.pid)
     i.kill()
